56542d3a2c19e6a241b644e0944773ae772a8921
skokorev <skokorev@gmail.com>
2020-03-30 13:57:06 +0300

    Minor fixes

--- provision/install.sh
+++ provision/install.sh
@@ -1,2 +1,6 @@
 sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install software-properties-common -y
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt-get update
 sudo apt-get install ansible -y
