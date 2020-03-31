657d90d397203fe7ba4f0ba9562f393c39eb7769
skokorev <skokorev@gmail.com>
2020-03-31 12:14:10 +0300

    Fixed to working instance

--- provision/install.sh
+++ provision/install.sh
@@ -1,6 +1,7 @@
-sudo apt-get update
-sudo apt-get upgrade -y
+sudo apt-get install dirmngr --install-recommends -y
 sudo apt-get install software-properties-common -y
-sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt-key add gpg
+sudo add-apt-repository "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 sudo apt-get update
-sudo apt-get install ansible -y
+sudo apt-get install ansible --allow-unauthenticated -y
